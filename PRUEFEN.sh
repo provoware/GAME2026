c15b9bbbed07028b72c6036da8f1e0f7aa5584e6
pppoppi <provoware.one@gmail.com>
2026-08-18 23:23:33 +0200

    VI-AW: Vollprüfung um Vertrag erweitern

--- PRUEFEN.sh
+++ PRUEFEN.sh
@@ -44,4 +44,5 @@ node web/tests/lc11-visual-polish-6ar-contract.test.js
 node web/tests/lc11-visual-polish-6at-contract.test.js
 node web/tests/lc11-visual-polish-6au-contract.test.js
 node web/tests/lc11-visual-polish-6av-contract.test.js
+node web/tests/lc11-visual-polish-6aw-contract.test.js
 echo 'ERGEBNIS: PASS'
